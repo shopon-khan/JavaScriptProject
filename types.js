4a3fe9cfeeb039c82221def92841b0ceb953bd49
shopon-khan <soponkhan212020@gmail.com>
2023-02-22 12:37:49 +0600

    to learn parseFlot and toFiexed

---
+++ types.js
@@ -0,0 +1,9 @@
+//advance 
+// parseFloat and toFiexed 
+//  kuno bognaonsho number ke fiexd  kore purno number convert kora
+var  num1 = 0.1;
+var num2 = 0.2;
+var num3 = num1 + num2;
+num3 = num3.toFixed(2);
+num3 = parseFloat(num3);
+console.log(num3);
