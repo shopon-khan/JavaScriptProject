95a48e4bc73acc8029ba7d5345ca2660ac900e80
shopon-khan <soponkhan212020@gmail.com>
2023-02-22 20:01:45 +0600

    previous day learning revise

---
+++ variable.js
@@ -0,0 +1,30 @@
+// numaric number
+var mangoPrice = 120;
+
+// string variable 
+var someOneName = "Shopon";
+
+// bollean variable 
+var ishappy = true;
+
+// type of number 2
+// integer = 1, 2, 3
+// float = 5.42
+
+/**
+ *  can not start keyword
+ * can not start number 
+ * can not use dersh
+ * can not use quoteation
+ * should use camelCase
+ * should use paseCase
+ * should use sankCase
+ * * */
+
+// Math operation
+// +, -, *, /
+// shorthand : +=, -+, ++, --
+
+// convert: parsent, parseFloat
+
+// remainder: vagshes %
