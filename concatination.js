a8a184e15e5b0d9974114058b816c5525694b79e
shopon-khan <soponkhan212020@gmail.com>
2023-02-22 12:18:53 +0600

    add conversion

---
+++ concatination.js
@@ -0,0 +1,5 @@
+// concatination means two words will be addintion 
+var firstName = "Lily";
+var lastname = "Akther";
+var fullName = firstName + ' ' + lastname;
+console.log(fullName);
---
+++ conversion.js
@@ -0,0 +1,26 @@
+// string to number convert only for work intizer number
+var price1 = 31;
+var price2 = "10";
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+var price1 = 31;
+var price2 = "10";
+var updateNumber = parseInt(price2);
+var totalPrice = price1 + updateNumber;
+// console.log(totalPrice);
+
+var mony1 = "40";
+var mony2 = 20;
+var moneyHoy = parseInt(mony1);
+var all = mony2 + moneyHoy;
+console.log(all);
+
+// convert float number like 1.25
+
+var number1 = 4.45;
+var number2 = 2;
+var convertN = parseFloat(number1);
+var number4 = number2 + convertN;
+console.log(number4); 
+
