f26b34f767178c0c01a3c9bba7b6b9115da4de8b
shopon-khan <soponkhan212020@gmail.com>
2023-02-22 12:47:21 +0600

    now learn reminder

---
+++ reminder.js
@@ -0,0 +1,12 @@
+// reminder means vagfol and vag shes ber kora jai
+// vag fol ber kore / aita diye 
+// vag shes % diye
+
+var manogs = 10;
+var person = 3;
+var remainder = manogs / person;
+// console.log(remainder);
+var manogs = 19;
+var person = 3;
+var remainder = manogs % person;
+console.log(remainder);
