65daa8d5968d56860a97ce6ced6a388994304447
shopon-khan <soponkhan212020@gmail.com>
2023-02-22 17:42:06 +0600

    practice problems

---
+++ practice.js
@@ -0,0 +1,24 @@
+// practice problem 1
+var harrysMomGiveHim = 1000;
+var OneKgApplePrice = 400;
+var OneKgOrangePrice = 300;
+var totalCostAmount = OneKgApplePrice + OneKgOrangePrice;
+var shopkeeperReturn = harrysMomGiveHim - totalCostAmount;
+// console.log(shopkeeperReturn); 
+
+// practice problem 2
+var marksOfFiveSubjects = 75.25 + 65 + 80 + 35.45 + 99.50;
+var totalMarks = marksOfFiveSubjects;
+var avarageMarks = totalMarks/5;
+var result = avarageMarks;
+ result = result.toFixed(2)                      
+result= parseFloat(result);
+// console.log(result);
+
+var marksOfSevenSubjects = 75.25 + 70.45 + 45 + 33.68 + 60.46 + 99.50 + 39.18;
+var totalSubjecstMarks = marksOfSevenSubjects;
+var AvarageMarks = totalSubjecstMarks / 7;
+var result = AvarageMarks;
+result = result.toFixed(2);
+result = parseFloat(result);
+console.log(result);
